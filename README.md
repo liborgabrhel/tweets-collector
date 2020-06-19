a2637153ceef14f2cc3acd46cf42424183aa77c3
Libor Gabrhel <gabrhel.libor@gmail.com>
2020-06-19 21:52:17 +0200

    Added script with config json files and basic README.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# tweets-collector
-tweets collector
+# Tweets Collector
+
+This is a simple python script to search tweets and to save them into a text file.
+
+To run this script download this repository and To run this script download this repository and follow these steps:
+
+## Setings
+
+1. In the `twitter-settings.json` file fill in your twitter `consumer_key`, `consumer_secret`, `access_key` and `access_secret`
+2. In the `search-query-parameters.json` file fill in the search parameters
+
+❗️ Query parameter `from_user` can be with or without `@` prefix.
+
+❗️ Query parameters `language`, `from_user`, `since` and `until` can be empty.
+
+❗️ Query parameters `since` and `until` must be in `yyyy-mm-dd` format and you can not search tweets older than 7 days.
+
+## Run script
+
+### macOS
+
+### Windows
+
+### Linux
+ 
+ 
+
+
---
+++ tweets-collector-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+#
+# Author: Libor Gabrhel
+# Program Name: Tweets Collector
+
+# Python libraries
+import tweepy
+import json
+
+# Loads twitter settings from external file
+with open('twitter-settings.json', 'r') as twitter_settings_content:
+    twitter_settings = json.load(twitter_settings_content)
+
+# Loads search query arguments from external file
+with open('search-query-parameters.json', 'r') as search_query_parameters_content:
+    search_query_parameters = json.load(search_query_parameters_content)
+
+# Sets the twitter api
+consumer_key = twitter_settings['consumer_key']
+consumer_secret = twitter_settings['consumer_secret']
+access_key = twitter_settings["access_key"]
+access_secret = twitter_settings["access_secret"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+# Main application function
+def main():
+    query_keywords = search_query_parameters['keywords']
+    query_from = ('from:' + search_query_parameters['from_user']) if search_query_parameters['from_user'] else ""
+    language = search_query_parameters['language']
+    since = search_query_parameters['since']
+    until = search_query_parameters['until']
+    tweets_limit = search_query_parameters['tweets_limit']
+
+    separator = " "
+
+    filtered_query_parameters= [query_parameter for query_parameter in [query_keywords, query_from] if query_parameter]
+    query = separator.join(filtered_query_parameters)
+
+    for status in tweepy.Cursor(api.search, q=query, tweet_mode='extended', lang=language, since=since, until=until).items(tweets_limit):
+        file_path = 'collected-tweets/' + str(status.id) + '.txt'
+        text = ''
+
+        if hasattr(status, "retweeted_status"):  # Check if it is a retweet
+            text = status.retweeted_status.full_text
+        else:
+            text = status.full_text
+
+        with open(file_path, "w") as file:
+            file.write(text)
+
+    print('🎉 Done!')
+
+# Runs main application function
+main()
